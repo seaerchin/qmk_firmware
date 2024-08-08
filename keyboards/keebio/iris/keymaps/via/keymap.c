10fce6c0cdabd1b6ed033caeb1c3462ac237a877
seaerchin <jiachin@open.gov.sg>
2024-08-09 04:15:28 +0800

    feat: achordion

--- keyboards/keebio/iris/keymaps/via/keymap.c
+++ keyboards/keebio/iris/keymaps/via/keymap.c
@@ -2,6 +2,38 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
+#include "features/achordion.h"
+
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+    if (!process_achordion(keycode, record)) {
+        return false;
+    }
+    // Your macros ...
+
+    return true;
+}
+
+void matrix_scan_user(void) {
+    achordion_task();
+}
+
+uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t* record) {
+    // If you quickly hold a tap-hold key after tapping it, the tap action is
+    // repeated. Key repeating is useful e.g. for Vim navigation keys, but can
+    // lead to missed triggers in fast typing. Here, returning 0 means we
+    // instead want to "force hold" and disable key repeating.
+    switch (keycode) {
+        case HOME_N:
+        // Repeating is useful for Vim navigation keys.
+        case QHOME_J:
+        case QHOME_K:
+        case QHOME_L:
+            return QUICK_TAP_TERM; // Enable key repeating.
+        default:
+            return 0; // Otherwise, force hold and disable key repeating.
+    }
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT(KC_LGUI, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, KC_LALT, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_DEL, KC_LCTL, LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G, KC_H, RSFT_T(KC_J), RCTL_T(KC_K), RALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT, KC_LSFT, LT(1, KC_Z), KC_X, KC_C, KC_V, KC_B, KC_NO, KC_NO, KC_N, KC_M, KC_COMM, KC_DOT, LT(1, KC_SLSH), KC_RSFT, LT(2, KC_ESC), LT(3, KC_BSPC), LT(4, KC_TAB), LT(5, KC_ENT), LT(6, KC_SPC), LT(7, KC_DEL)),
                                                               [1] = LAYOUT(QK_BOOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_UNDO, KC_CUT, KC_COPY, KC_PSTE, KC_AGIN, KC_AGIN, KC_PSTE, KC_COPY, KC_CUT, KC_UNDO, KC_TRNS, KC_TRNS, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_TRNS, KC_TRNS, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, KC_TRNS, KC_TRNS, KC_UNDO, KC_CUT, KC_COPY, KC_PSTE, KC_AGIN, KC_BTN2, KC_BTN2, KC_AGIN, KC_PSTE, KC_COPY, KC_CUT, KC_UNDO, KC_TRNS, KC_BTN3, KC_BTN1, KC_BTN2, KC_BTN2, KC_BTN1, KC_BTN3),
                                                               [2] = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_CIRC, KC_WBAK, KC_WSTP, KC_WREF, KC_WFWD, KC_TRNS, KC_TRNS, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_TRNS, KC_EQL, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PLUS, KC_WSCH, KC_WHOM, KC_BRIU, KC_BRID, KC_TRNS, KC_NO, KC_TRNS, KC_TRNS, KC_MSTP, KC_MPLY, KC_MUTE),
--- keyboards/keebio/iris/keymaps/via/rules.mk
+++ keyboards/keebio/iris/keymaps/via/rules.mk
@@ -2,3 +2,5 @@ VIA_ENABLE = yes
 LTO_ENABLE = yes
 ENCODER_MAP_ENABLE = no
 CONSOLE_ENABLE = no
+SRC += features/achordion.c
+
